9c66240995b1a9f9482d7f68ad4ce32b9cbdfb18
Iman Haqim <imanhqm@protonmail.com>
2023-11-11 13:55:08 +0800

    Add basic arithmetic function

---
+++ script.js
@@ -0,0 +1,39 @@
+//to update UI
+let firstNumber;
+let operator;
+let secondNumber;
+
+function add (a, b) {
+    return a + b;
+};
+
+function subtract(a, b) {
+    return a - b;
+};
+
+function multiply(a, b) {
+    return a * b;
+};
+
+function divide(a, b) {
+    return a/b;
+};
+
+function operate (firstNumber, operator, secondNumber) {
+    switch (operator) {
+        case '+':
+            //
+            break;
+        case '-':
+            //
+            break;
+        case 'x':
+            //
+            break;
+        case '÷':
+            //
+            break;
+        default:
+            alert('INVALID OPERATOR');
+    }
+};
